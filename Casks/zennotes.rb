f335635407b1450913bc3c005fc9244ac657b51d
Adib Hanna <adibhanna@gmail.com>
2026-09-02 14:17:23 -0500

    zennotes 2.43.0

--- Casks/zennotes.rb
+++ Casks/zennotes.rb
@@ -1,9 +1,9 @@
 cask "zennotes" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.42.0"
-  sha256 arm:   "40589c52f9d369452cb03cefd66a97631d6498f4f03d39702f8849db5a78c51e",
-         intel: "6921bc87a4a4de974388b1c006ae3b65d051c95eeb798abc7c6568861d7fd64d"
+  version "2.43.0"
+  sha256 arm:   "216bd2c85d66dfdb59cf2b15f5b784cd2d9bf9e728ea40b075a3e4a507b1d89d",
+         intel: "5ed1199bda96cc93b639a9b7cce016a9110b8c2431fd3dd65f874fdbea3d53e8"
 
   url "https://github.com/ZenNotes/zennotes/releases/download/v#{version}/ZenNotes-#{version}-mac-#{arch}.dmg"
   name "ZenNotes"
